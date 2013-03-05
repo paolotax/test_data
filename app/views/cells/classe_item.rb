8d47303f23516552edfd51706419be541395315c
paolotax <paolo.tassinari@gmail.com>
2013-03-05 11:54:44 +0100

    classi adozioni

--- app/views/cells/classe_item.rb
+++ app/views/cells/classe_item.rb
@@ -8,62 +8,66 @@ class ClasseItem < UICollectionViewCell
       
       cell.autoresizingMask = UIViewAutoresizingFlexibleBottomMargin | UIViewAutoresizingFlexibleTopMargin | UIViewAutoresizingFlexibleLeftMargin | UIViewAutoresizingFlexibleRightMargin
       
-      @classe_label = UILabel.alloc.initWithFrame([[0, 20], [80, 30]]).tap do |label|
+      @classe_label = UILabel.alloc.initWithFrame([[0, 0], [55, 50]]).tap do |label|
          #label.translatesAutoresizingMaskIntoConstraints = false
          label.numberOfLines = 1
          label.font = UIFont.boldSystemFontOfSize(20.0)
          label.textAlignment = UITextAlignmentCenter
          label.backgroundColor = UIColor.clearColor
-         label.textColor = UIColor.whiteColor
+         label.textColor = UIColor.darkGrayColor
          label.autoresizingMask = UIViewAutoresizingFlexibleBottomMargin
          cell.contentView.addSubview(label)
       end
 
-      @alunni_label = UILabel.alloc.initWithFrame([[0, 55], [75, 20]]).tap do |label|
+      @alunni_label = UILabel.alloc.initWithFrame([[0, 0], [157, 20]]).tap do |label|
          #label.translatesAutoresizingMaskIntoConstraints = false
          label.numberOfLines = 1
          label.font = UIFont.systemFontOfSize(12.0)
          label.textAlignment = UITextAlignmentRight
          label.backgroundColor = UIColor.clearColor
-         label.textColor = UIColor.whiteColor
+         label.textColor = UIColor.darkGrayColor
          label.autoresizingMask = UIViewAutoresizingFlexibleBottomMargin
          cell.contentView.addSubview(label)
       end
 
-      @adozione_image =  UIImageView.alloc.initWithFrame([[5, 53], [20, 25]]).tap do |imgv|
+      @adozione_image =  UIImageView.alloc.initWithFrame([[5, 50], [50, 60]]).tap do |imgv|
         #imgv.translatesAutoresizingMaskIntoConstraints = false
         imgv.autoresizingMask = UIViewAutoresizingFlexibleBottomMargin | UIViewAutoresizingFlexibleRightMargin
         imgv.backgroundColor = UIColor.clearColor
-
         imgv.layer.cornerRadius = 5
+        imgv.alpha = 0.8
         imgv.layer.masksToBounds = true
+        self.contentView.addSubview(imgv)
+      end
 
+      @adozione_image_2 =  UIImageView.alloc.initWithFrame([[55, 50], [50, 60]]).tap do |imgv|
+        #imgv.translatesAutoresizingMaskIntoConstraints = false
+        imgv.autoresizingMask = UIViewAutoresizingFlexibleBottomMargin | UIViewAutoresizingFlexibleRightMargin
+        imgv.backgroundColor = UIColor.clearColor
+        imgv.layer.cornerRadius = 5
+        imgv.alpha = 0.8
+        imgv.layer.masksToBounds = true
+        self.contentView.addSubview(imgv)
+      end
 
+      @adozione_image_3 =  UIImageView.alloc.initWithFrame([[105, 50], [50, 60]]).tap do |imgv|
+        #imgv.translatesAutoresizingMaskIntoConstraints = false
+        imgv.autoresizingMask = UIViewAutoresizingFlexibleBottomMargin | UIViewAutoresizingFlexibleRightMargin
+        imgv.backgroundColor = UIColor.clearColor
+        imgv.layer.cornerRadius = 5
+        imgv.alpha = 0.8
+        imgv.layer.masksToBounds = true
         self.contentView.addSubview(imgv)
       end
 
       cell.contentView.autoresizesSubviews = true
 
-      # bg = "bgclasse.png".uiimage
-      # bgView = UIImageView.alloc.initWithImage bg
-      # cell.backgroundView = bgView
+      # cell.backgroundColor = UIColor.clearColor
+      # image = UIImage.imageNamed("bgclasse.png")
+      # cell.layer.backgroundColor = UIColor.colorWithPatternImage(image).CGColor;
+      cell.backgroundColor = UIColor.whiteColor
 
       # cell.clipsToBounds = false
-
-      # cell.layer.borderColor = UIColor.yellowColor.CGColor
-      # cell.layer.borderWidth = 3
-      # cell.layer.cornerRadius = 8
-      # cell.layer.masksToBounds = true
-
-      # cell.layer.shadowColor = UIColor.blackColor.CGColor
-      # cell.layer.shadowOffset = CGSizeMake(3.0,3.0)
-      # cell.layer.shadowOpacity = 0.5
-      # cell.layer.shadowRadius = 3.0
-
-
-      cell.backgroundColor = UIColor.clearColor
-      image = UIImage.imageNamed("bgclasse.png")
-      cell.layer.backgroundColor = UIColor.colorWithPatternImage(image).CGColor;
 
       # Rounded corners.
       cell.layer.cornerRadius = 10
@@ -88,18 +92,55 @@ class ClasseItem < UICollectionViewCell
       @classe_label.text = "#{@classe.num_classe} #{@classe.sezione}"
       @alunni_label.text = "#{@classe.nr_alunni}b"
 
-      
-      unless @classe.adozioni.count == 0
+      mask = "mask-adozione".uiimage
+
+      if @classe.adozioni.count == 1
         @adozione_image.hidden = false
-        @adozione_image.setImage UIImage.imageNamed("#{@classe.adozioni.objectAtIndex(0).sigla}.jpeg")
+        @adozione_image.setImage(self.appImage(UIImage.imageNamed("#{@classe.adozioni.objectAtIndex(0).sigla}.jpeg"), mask))   
+        @adozione_image_2.hidden = true
+        @adozione_image_3.hidden = true
+      
+      elsif @classe.adozioni.count == 2 
+        @adozione_image.setImage(self.appImage(UIImage.imageNamed("#{@classe.adozioni.objectAtIndex(0).sigla}.jpeg"), mask))   
+        @adozione_image_2.setImage(self.appImage(UIImage.imageNamed("#{@classe.adozioni.objectAtIndex(1).sigla}.jpeg"), mask))
+        @adozione_image.hidden = false
+        @adozione_image_2.hidden = false
+        @adozione_image_3.hidden = true
+
+      elsif @classe.adozioni.count >= 3 
+        @adozione_image.setImage(self.appImage(UIImage.imageNamed("#{@classe.adozioni.objectAtIndex(0).sigla}.jpeg"), mask))   
+        @adozione_image_2.setImage(self.appImage(UIImage.imageNamed("#{@classe.adozioni.objectAtIndex(1).sigla}.jpeg"), mask))  
+        @adozione_image_3.setImage(self.appImage(UIImage.imageNamed("#{@classe.adozioni.objectAtIndex(2).sigla}.jpeg"), mask))
+        @adozione_image.hidden = false
+        @adozione_image_2.hidden = false
+        @adozione_image_3.hidden = false
+ 
       else
         @adozione_image.hidden = true
+        @adozione_image_2.hidden = true
+        @adozione_image_3.hidden = true
       end
 
     end
     @classe
   end
 
+  def appImage(image, maskImage)
+    
+    if image
+      maskRef = maskImage.CGImage
+      mask = CGImageMaskCreate(CGImageGetWidth(maskRef),
+              CGImageGetHeight(maskRef),
+              CGImageGetBitsPerComponent(maskRef),
+              CGImageGetBitsPerPixel(maskRef),
+              CGImageGetBytesPerRow(maskRef),
+              CGImageGetDataProvider(maskRef), nil, false)
+   
+      masked = CGImageCreateWithMask(image.CGImage, mask)
+      return UIImage.imageWithCGImage(masked)
+    end
+  end
+ 
 
 
 end
